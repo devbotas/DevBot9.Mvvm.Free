0177c7054cfca5150e7a7ac27f7f79bafe6634d3
devbotas <simon@devbot.lt>
2022-05-05 17:07:26 +0300

    Removing Tests, VB and .NET projects.

--- DevExpress.Mvvm.Tests/Views/EventToCommandTestView.xaml.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Controls;
-
-namespace DevExpress.Mvvm.UI.Tests {
-    public partial class EventToCommandTestView : UserControl {
-        public EventToCommandTestView() {
-            InitializeComponent();
-        }
-    }
-}
