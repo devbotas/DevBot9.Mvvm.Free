26a2f87880df128fac217e0e391794799be1a652
devbotas <simon@devbot.lt>
2022-05-16 13:55:23 +0300

    Removing assembly signing.

--- DevExpress.Mvvm/Native/NativeConstants.cs
+++ DevExpress.Mvvm/Native/NativeConstants.cs
@@ -24,9 +24,8 @@ namespace DevBot9.Mvvm.Native {
         public const string GanttNamespaceDefinition = "http://schemas.devexpress.com/winfx/2008/xaml/gantt";
     }
     static class MvvmAssemblyHelper {
-        const string PublicKey = "PublicKey=0024000004800000940000000602000000240000525341310004000001000100c9634fb4bd8481aa329ba0fbb6fe72063462135ecfb3c8a8a595ce4436df2697f258912e9aa0f705fabfb1d13d6044a55a42cee9fe81bcdce571a4fbeeaa758e1d4a2a20ffd8ea05018133be71da27951f0103ab3cec55f78009ff0d00fb8b02db756437ecee8893c624c8f1c639f342ceda43dd8a443460ca93d0d2990904bb";
-        public const string TestsAssemblyName = "DevBot9.Mvvm.Tests, " + PublicKey;
-        public const string TestsFreeAssemblyName = "DevBot9.Mvvm.Tests.Free, " + PublicKey;
-        public const string MvvmUIAssemblyName = "DevBot9.Mvvm.UI, " + PublicKey;
+        public const string TestsAssemblyName = "DevBot9.Mvvm.Tests";
+        public const string TestsFreeAssemblyName = "DevBot9.Mvvm.Tests.Free";
+        public const string MvvmUIAssemblyName = "DevBot9.Mvvm.UI";
     }
 }
